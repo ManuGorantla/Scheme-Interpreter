b1ca1e7c34277609bacd927535333a2ac40b07df
ManuGorantla <163234385+ManuGorantla@users.noreply.github.com>
2025-01-28 17:09:25 -0800

    Update pair.py

--- pair.py
+++ pair.py
@@ -1,14 +1,5 @@
 class Pair:
-    """A pair has two instance attributes: first and rest. rest must be a Pair or nil
 
-    >>> s = Pair(1, Pair(2, nil))
-    >>> s
-    Pair(1, Pair(2, nil))
-    >>> print(s)
-    (1 2)
-    >>> print(s.map(lambda x: x+4))
-    (5 6)
-    """
     def __init__(self, first, rest):
         self.first = first
         self.rest = rest
@@ -59,7 +50,6 @@ class Pair:
 
 
 class nil:
-    """The empty list"""
 
     def __repr__(self):
         return 'nil'
@@ -76,10 +66,9 @@ class nil:
     def flatmap(self, fn):
         return self
 
-nil = nil() # Assignment hides the nil class; there is only one instance
+nil = nil() 
 
 def repl_str(val):
-    """Should largely match str(val), except for booleans and undefined."""
     if val is True:
         return "#t"
     if val is False:
